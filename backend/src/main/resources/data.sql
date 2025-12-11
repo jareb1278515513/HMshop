74710c538b5899191460eb09673dd8ace148843a
jareb1278515513 <liujialunbupt@bupt.edu.cn>
2025-12-12 01:10:32 +0800

    增加了几个商品和商品图片

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,7 @@
+-- 设置全局图片路径变量 (修改此处即可切换IP)
+SET @img_base = 'http://10.21.206.207:5344/img/';
+SET @old_base = 'http://hmapp.net/static/img/';
+
 INSERT INTO shop_user (id, username, password, nickname, avatar, mobile, gender, add_time, update_time)
 VALUES (1, 'demo', '123456', '演示用户', 'http://hmapp.net/static/img/avatar.png', '13800138000', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
@@ -12,29 +16,29 @@ INSERT INTO category (id, name, keywords, description, pid, icon_url, pic_url, l
 VALUES (1008002, '手机通讯', '手机', '旗舰机与性价比', 1008000, 'http://hmapp.net/static/img/cat_icon2.png', 'http://hmapp.net/static/img/cat_pic2.png', 'L2', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
 
 INSERT INTO brand (id, name, description, pic_url, sort_order, floor_price, add_time, update_time, deleted)
-VALUES (1001000, 'Harmony 品牌', '鸿蒙生态精品', 'http://hmapp.net/static/img/brand.png', 1, 1999, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+VALUES (1001000, 'HUAWEI', '华为终端产品', 'http://hmapp.net/static/img/brand.png', 1, 1999, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
 
 INSERT INTO goods (id, goods_sn, name, category_id, brand_id, gallery, keywords, brief, is_on_sale, sort_order, pic_url, share_url, is_new, is_hot, unit, counter_price, retail_price, detail, add_time, update_time, deleted)
-VALUES (1055016, 'HM-001', 'Harmony Phone X', 1008002, 1001000,
+VALUES (1055016, 'HM-001', 'HUAWEI Mate 80 Pro', 1008002, 1001000,
         'http://hmapp.net/static/img/goods1.png,http://hmapp.net/static/img/goods1b.png,http://hmapp.net/static/img/goods1c.png',
-        '手机,鸿蒙', '旗舰鸿蒙手机', TRUE, 1, 'http://hmapp.net/static/img/goods1.png', 'http://hmapp.net/static/img/goods1.png',
-        TRUE, TRUE, '台', 3599, 3299,
-        '<p>Harmony Phone X 采用鸿蒙系统，旗舰配置。</p><img src=\"http://hmapp.net/static/img/goods1.png\"/>',
+        '手机,Mate', '超可靠玄武架构，双卫星通信', TRUE, 1, 'http://hmapp.net/static/img/goods1.png', 'http://hmapp.net/static/img/goods1.png',
+        TRUE, TRUE, '台', 6999, 6999,
+        '<p>HUAWEI Mate 80 Pro 搭载鸿蒙操作系统4.0，支持卫星通话。</p><img src=\"http://hmapp.net/static/img/goods1.png\"/>',
         CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
 
 INSERT INTO goods_attribute (goods_id, attribute, attr_value, add_time, update_time, deleted)
-VALUES (1055016, '屏幕', '6.7英寸 OLED', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+VALUES (1055016, '屏幕', '6.82英寸 OLED', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
        (1055016, '电池', '5000mAh', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
 
 INSERT INTO goods_specification (id, goods_id, specification, spec_value, pic_url, add_time, update_time, deleted)
-VALUES (50101, 1055016, '颜色', '曜石黑', 'http://hmapp.net/static/img/goods1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
-       (50102, 1055016, '颜色', '皓月白', 'http://hmapp.net/static/img/goods1b.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
-       (50103, 1055016, '存储', '128GB', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
-       (50104, 1055016, '存储', '256GB', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+VALUES (50101, 1055016, '颜色', '云杉绿', 'http://hmapp.net/static/img/goods1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50102, 1055016, '颜色', '白沙银', 'http://hmapp.net/static/img/goods1b.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50103, 1055016, '存储', '512GB', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50104, 1055016, '存储', '1TB', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
 
 INSERT INTO goods_product (id, goods_id, specifications, price, number, url, add_time, update_time, deleted)
-VALUES (60101, 1055016, '曜石黑,128GB', 3299, 50, 'http://hmapp.net/static/img/goods1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
-       (60102, 1055016, '皓月白,256GB', 3599, 30, 'http://hmapp.net/static/img/goods1b.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+VALUES (60101, 1055016, '云杉绿,512GB', 6999, 50, 'http://hmapp.net/static/img/goods1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (60102, 1055016, '白沙银,1TB', 7999, 30, 'http://hmapp.net/static/img/goods1b.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
 
 INSERT INTO banner (name, link, url, position, content, enabled, add_time, update_time, deleted)
 VALUES ('新品首发', '/pages/DetailPage?did=1055016', 'http://hmapp.net/static/img/banner1.png', 1, 'Harmony X', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
@@ -73,3 +77,153 @@ VALUES (3001, 1055016, 200, 2, DATEADD('DAY', 15, CURRENT_TIMESTAMP), TRUE);
 
 INSERT INTO groupon_record (order_id, rules_id, user_id, status, add_time)
 VALUES (7001, 3001, 1, 1, CURRENT_TIMESTAMP);
+
+-- 新增分类：智能家居
+INSERT INTO category (id, name, keywords, description, pid, icon_url, pic_url, level, sort_order, add_time, update_time, deleted)
+VALUES (1008010, '智能家居', '智慧屏,音箱', '打造智慧生活', 0, 'http://hmapp.net/static/img/cat_icon_home.png', 'http://hmapp.net/static/img/cat_pic_home.png', 'L1', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO category (id, name, keywords, description, pid, icon_url, pic_url, level, sort_order, add_time, update_time, deleted)
+VALUES (1008011, '智慧屏', '电视,智慧屏', '家庭娱乐中心', 1008010, 'http://hmapp.net/static/img/cat_icon_tv.png', 'http://hmapp.net/static/img/cat_pic_tv.png', 'L2', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO category (id, name, keywords, description, pid, icon_url, pic_url, level, sort_order, add_time, update_time, deleted)
+VALUES (1008012, '智能音箱', '音箱,Sound', '高保真音质', 1008010, 'http://hmapp.net/static/img/cat_icon_speaker.png', 'http://hmapp.net/static/img/cat_pic_speaker.png', 'L2', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增品牌：SoundX
+INSERT INTO brand (id, name, description, pic_url, sort_order, floor_price, add_time, update_time, deleted)
+VALUES (1001001, 'SoundX', '极致声学体验', 'http://hmapp.net/static/img/brand_sound.png', 2, 999, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增商品：HUAWEI Vision S 86 (智慧屏)
+INSERT INTO goods (id, goods_sn, name, category_id, brand_id, gallery, keywords, brief, is_on_sale, sort_order, pic_url, share_url, is_new, is_hot, unit, counter_price, retail_price, detail, add_time, update_time, deleted)
+VALUES (1055030, 'HM-TV-001', 'HUAWEI Vision S 86', 1008011, 1001000,
+        'http://hmapp.net/static/img/goods_tv1.png,http://hmapp.net/static/img/goods_tv2.png',
+        '智慧屏,电视', '120Hz鸿蒙炫彩屏', TRUE, 2, 'http://hmapp.net/static/img/goods_tv1.png', 'http://hmapp.net/static/img/goods_tv1.png',
+        TRUE, TRUE, '台', 6499, 5999,
+        '<p>HUAWEI Vision S 86 带来沉浸式视听体验。</p><img src=\"http://hmapp.net/static/img/goods_tv1.png\"/>',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增商品：HUAWEI Sound X NEW (智能音箱)
+INSERT INTO goods (id, goods_sn, name, category_id, brand_id, gallery, keywords, brief, is_on_sale, sort_order, pic_url, share_url, is_new, is_hot, unit, counter_price, retail_price, detail, add_time, update_time, deleted)
+VALUES (1055031, 'HM-SPK-001', 'HUAWEI Sound X NEW', 1008012, 1001001,
+        'http://hmapp.net/static/img/goods_spk1.png,http://hmapp.net/static/img/goods_spk2.png',
+        '音箱,HiFi', '帝瓦雷联合设计，三分频智慧音箱', TRUE, 3, 'http://hmapp.net/static/img/goods_spk1.png', 'http://hmapp.net/static/img/goods_spk1.png',
+        TRUE, TRUE, '台', 2199, 1999,
+        '<p>HUAWEI Sound X NEW 震撼音质，一碰传音。</p><img src=\"http://hmapp.net/static/img/goods_spk1.png\"/>',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 商品参数
+INSERT INTO goods_attribute (goods_id, attribute, attr_value, add_time, update_time, deleted)
+VALUES (1055030, '屏幕尺寸', '86英寸', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (1055030, '分辨率', '4K', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (1055031, '扬声器', '8单元三分频', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (1055031, '连接', '蓝牙5.0/WiFi', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 商品规格
+INSERT INTO goods_specification (id, goods_id, specification, spec_value, pic_url, add_time, update_time, deleted)
+VALUES (50201, 1055030, '安装方式', '挂架式', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50202, 1055030, '安装方式', '底座式', '', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50301, 1055031, '颜色', '韵律蓝', 'http://hmapp.net/static/img/goods_spk1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50302, 1055031, '颜色', '鎏金黑', 'http://hmapp.net/static/img/goods_spk2.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 商品库存 (Product)
+INSERT INTO goods_product (id, goods_id, specifications, price, number, url, add_time, update_time, deleted)
+VALUES (60201, 1055030, '挂架式', 5999, 100, 'http://hmapp.net/static/img/goods_tv1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (60202, 1055030, '底座式', 6099, 50, 'http://hmapp.net/static/img/goods_tv1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (60301, 1055031, '韵律蓝', 1999, 200, 'http://hmapp.net/static/img/goods_spk1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (60302, 1055031, '鎏金黑', 1999, 150, 'http://hmapp.net/static/img/goods_spk2.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增 Banner
+INSERT INTO banner (name, link, url, position, content, enabled, add_time, update_time, deleted)
+VALUES ('智慧生活节', '/pages/CategoryPage?cid=1008010', 'http://hmapp.net/static/img/banner_home.png', 2, 'Smart Home', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增 Topic
+INSERT INTO topic (title, subtitle, price, read_count, pic_url, sort_order, goods, add_time, update_time, deleted, content)
+VALUES ('打造智慧家', '全屋智能方案', 9999, '5k+', 'http://hmapp.net/static/img/topic_home.png', 2, '[1055030, 1055031]', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE, '<p>从客厅到卧室，全屋智能体验。</p>');
+
+-- 新增 Coupon
+INSERT INTO coupon (id, name, description, tag, discount, min, start_time, end_time, days, enabled)
+VALUES (3, '智能家居券', '仅限智能家居品类', '智能', 100, 1000, CURRENT_TIMESTAMP, DATEADD('DAY', 30, CURRENT_TIMESTAMP), 0, TRUE);
+
+-- 新增分类：电脑办公
+INSERT INTO category (id, name, keywords, description, pid, icon_url, pic_url, level, sort_order, add_time, update_time, deleted)
+VALUES (1008003, '电脑办公', '笔记本,台式机', '高效生产力', 1008000, 'http://hmapp.net/static/img/cat_icon_pc.png', 'http://hmapp.net/static/img/cat_pic_pc.png', 'L2', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增分类：平板电脑
+INSERT INTO category (id, name, keywords, description, pid, icon_url, pic_url, level, sort_order, add_time, update_time, deleted)
+VALUES (1008004, '平板电脑', '平板,MatePad', '创造力工具', 1008000, 'http://hmapp.net/static/img/cat_icon_pad.png', 'http://hmapp.net/static/img/cat_pic_pad.png', 'L2', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增商品：HUAWEI Mate XT 非凡大师 (三折叠)
+INSERT INTO goods (id, goods_sn, name, category_id, brand_id, gallery, keywords, brief, is_on_sale, sort_order, pic_url, share_url, is_new, is_hot, unit, counter_price, retail_price, detail, add_time, update_time, deleted)
+VALUES (1055040, 'HM-XT-001', 'HUAWEI Mate XT 非凡大师', 1008002, 1001000,
+        'http://hmapp.net/static/img/goods_xt1.png,http://hmapp.net/static/img/goods_xt2.png',
+        '三折叠,非凡大师', '全球首款三折叠屏手机', TRUE, 1, 'http://hmapp.net/static/img/goods_xt1.png', 'http://hmapp.net/static/img/goods_xt1.png',
+        TRUE, TRUE, '台', 19999, 19999,
+        '<p>HUAWEI Mate XT 非凡大师，展开即是10.2英寸大屏。</p><img src=\"http://hmapp.net/static/img/goods_xt1.png\"/>',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_attribute (goods_id, attribute, attr_value, add_time, update_time, deleted)
+VALUES (1055040, '屏幕', '10.2英寸 三折叠', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (1055040, '厚度', '展开3.6mm', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_specification (id, goods_id, specification, spec_value, pic_url, add_time, update_time, deleted)
+VALUES (50401, 1055040, '颜色', '瑞红', 'http://hmapp.net/static/img/goods_xt1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50402, 1055040, '颜色', '玄黑', 'http://hmapp.net/static/img/goods_xt2.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_product (id, goods_id, specifications, price, number, url, add_time, update_time, deleted)
+VALUES (60401, 1055040, '瑞红', 19999, 10, 'http://hmapp.net/static/img/goods_xt1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (60402, 1055040, '玄黑', 19999, 10, 'http://hmapp.net/static/img/goods_xt2.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增商品：HUAWEI MateBook X Pro (电脑)
+INSERT INTO goods (id, goods_sn, name, category_id, brand_id, gallery, keywords, brief, is_on_sale, sort_order, pic_url, share_url, is_new, is_hot, unit, counter_price, retail_price, detail, add_time, update_time, deleted)
+VALUES (1055041, 'HM-PC-001', 'HUAWEI MateBook X Pro', 1008003, 1001000,
+        'http://hmapp.net/static/img/goods_pc1.png,http://hmapp.net/static/img/goods_pc2.png',
+        '笔记本,轻薄本', '980g超轻薄，高性能', TRUE, 2, 'http://hmapp.net/static/img/goods_pc1.png', 'http://hmapp.net/static/img/goods_pc1.png',
+        TRUE, TRUE, '台', 11199, 10999,
+        '<p>HUAWEI MateBook X Pro 开启高性能轻薄本新时代。</p><img src=\"http://hmapp.net/static/img/goods_pc1.png\"/>',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_attribute (goods_id, attribute, attr_value, add_time, update_time, deleted)
+VALUES (1055041, '处理器', 'Core Ultra 9', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (1055041, '重量', '980g', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_specification (id, goods_id, specification, spec_value, pic_url, add_time, update_time, deleted)
+VALUES (50501, 1055041, '颜色', '晴蓝', 'http://hmapp.net/static/img/goods_pc1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50502, 1055041, '颜色', '砚黑', 'http://hmapp.net/static/img/goods_pc2.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_product (id, goods_id, specifications, price, number, url, add_time, update_time, deleted)
+VALUES (60501, 1055041, '晴蓝', 10999, 50, 'http://hmapp.net/static/img/goods_pc1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (60502, 1055041, '砚黑', 10999, 50, 'http://hmapp.net/static/img/goods_pc2.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 新增商品：HUAWEI MatePad Pro 13.2 (平板)
+INSERT INTO goods (id, goods_sn, name, category_id, brand_id, gallery, keywords, brief, is_on_sale, sort_order, pic_url, share_url, is_new, is_hot, unit, counter_price, retail_price, detail, add_time, update_time, deleted)
+VALUES (1055042, 'HM-PAD-001', 'HUAWEI MatePad Pro 13.2', 1008004, 1001000,
+        'http://hmapp.net/static/img/goods_pad1.png,http://hmapp.net/static/img/goods_pad2.png',
+        '平板,OLED', '13.2英寸柔性OLED屏', TRUE, 3, 'http://hmapp.net/static/img/goods_pad1.png', 'http://hmapp.net/static/img/goods_pad1.png',
+        TRUE, TRUE, '台', 5199, 4999,
+        '<p>HUAWEI MatePad Pro 13.2 刷新平板轻薄极限。</p><img src=\"http://hmapp.net/static/img/goods_pad1.png\"/>',
+        CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_attribute (goods_id, attribute, attr_value, add_time, update_time, deleted)
+VALUES (1055042, '屏幕', '13.2英寸 OLED', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (1055042, '厚度', '5.5mm', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_specification (id, goods_id, specification, spec_value, pic_url, add_time, update_time, deleted)
+VALUES (50601, 1055042, '颜色', '雅川青', 'http://hmapp.net/static/img/goods_pad1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (50602, 1055042, '颜色', '曜金黑', 'http://hmapp.net/static/img/goods_pad2.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+INSERT INTO goods_product (id, goods_id, specifications, price, number, url, add_time, update_time, deleted)
+VALUES (60601, 1055042, '雅川青', 4999, 100, 'http://hmapp.net/static/img/goods_pad1.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE),
+       (60602, 1055042, '曜金黑', 4999, 100, 'http://hmapp.net/static/img/goods_pad2.png', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, FALSE);
+
+-- 批量更新图片路径 (将默认域名替换为配置的IP)
+UPDATE shop_user SET avatar = REPLACE(avatar, @old_base, @img_base);
+UPDATE category SET icon_url = REPLACE(icon_url, @old_base, @img_base), pic_url = REPLACE(pic_url, @old_base, @img_base);
+UPDATE brand SET pic_url = REPLACE(pic_url, @old_base, @img_base);
+UPDATE goods SET pic_url = REPLACE(pic_url, @old_base, @img_base), share_url = REPLACE(share_url, @old_base, @img_base), gallery = REPLACE(gallery, @old_base, @img_base), detail = REPLACE(detail, @old_base, @img_base);
+UPDATE goods_specification SET pic_url = REPLACE(pic_url, @old_base, @img_base);
+UPDATE goods_product SET url = REPLACE(url, @old_base, @img_base);
+UPDATE banner SET url = REPLACE(url, @old_base, @img_base);
+UPDATE channel SET icon_url = REPLACE(icon_url, @old_base, @img_base);
+UPDATE topic SET pic_url = REPLACE(pic_url, @old_base, @img_base);
+UPDATE cart SET pic_url = REPLACE(pic_url, @old_base, @img_base);
+UPDATE order_item SET pic_url = REPLACE(pic_url, @old_base, @img_base);
+
---
+++ backend/target/classes/static/img/need.md
@@ -0,0 +1,64 @@
+# 项目完整图片资源清单
+
+请将以下所有图片文件放入后端项目的静态资源目录：
+`backend/src/main/resources/static/img/`
+
+> **提示**：此清单包含了系统运行所需的所有图片。请确保文件名完全一致（区分大小写）。
+
+## 1. 系统与UI图标 (System & UI)
+| 图片文件名 | 用途描述 | 建议尺寸 |
+| :--- | :--- | :--- |
+| **`avatar.png`** | 默认用户头像 | 200x200 |
+| **`icon_phone.png`** | 首页金刚区图标 - 手机 | 100x100 |
+| **`icon_parts.png`** | 首页金刚区图标 - 配件 | 100x100 |
+| **`icon_coupon.png`** | 首页金刚区图标 - 优惠 | 100x100 |
+| **`icon_vip.png`** | 首页金刚区图标 - 会员 | 100x100 |
+
+## 2. 分类图片 (Categories)
+| 图片文件名 | 用途描述 | 建议尺寸 |
+| :--- | :--- | :--- |
+| **`cat_icon.png`** | 一级分类图标 - 数码电器 | 100x100 |
+| **`cat_pic.png`** | 一级分类Banner - 数码电器 | 300x200 |
+| **`cat_icon2.png`** | 二级分类图标 - 手机通讯 | 100x100 |
+| **`cat_pic2.png`** | 二级分类Banner - 手机通讯 | 300x200 |
+| **`cat_icon_home.png`** | 一级分类图标 - 智能家居 | 100x100 |
+| **`cat_pic_home.png`** | 一级分类Banner - 智能家居 | 300x200 |
+| **`cat_icon_tv.png`** | 二级分类图标 - 智慧屏 | 100x100 |
+| **`cat_pic_tv.png`** | 二级分类Banner - 智慧屏 | 300x200 |
+| **`cat_icon_speaker.png`** | 二级分类图标 - 智能音箱 | 100x100 |
+| **`cat_pic_speaker.png`** | 二级分类Banner - 智能音箱 | 300x200 |
+| **`cat_icon_pc.png`** | 二级分类图标 - 电脑办公 | 100x100 |
+| **`cat_pic_pc.png`** | 二级分类Banner - 电脑办公 | 300x200 |
+| **`cat_icon_pad.png`** | 二级分类图标 - 平板电脑 | 100x100 |
+| **`cat_pic_pad.png`** | 二级分类Banner - 平板电脑 | 300x200 |
+
+## 3. 品牌图片 (Brands)
+| 图片文件名 | 用途描述 | 建议尺寸 |
+| :--- | :--- | :--- |
+| **`brand.png`** | 品牌Logo - HUAWEI | 200x100 |
+| **`brand_sound.png`** | 品牌Logo - SoundX | 200x100 |
+
+## 4. 商品图片 (Goods)
+| 图片文件名 | 对应商品 | 用途描述 | 建议尺寸 |
+| :--- | :--- | :--- | :--- |
+| **`goods1.png`** | Mate 80 Pro | 主图 / 雅川青 | 800x800 |
+| **`goods1b.png`** | Mate 80 Pro | 附图 / 白沙银 | 800x800 |
+| **`goods1c.png`** | Mate 80 Pro | 细节图 | 800x800 |
+| **`goods_tv1.png`** | Vision S 86 | 主图 / 挂架式 | 800x800 |
+| **`goods_tv2.png`** | Vision S 86 | 附图 / 底座式 | 800x800 |
+| **`goods_spk1.png`** | Sound X NEW | 主图 / 韵律蓝 | 800x800 |
+| **`goods_spk2.png`** | Sound X NEW | 附图 / 鎏金黑 | 800x800 |
+| **`goods_xt1.png`** | Mate XT | 主图 / 瑞红 | 800x800 |
+| **`goods_xt2.png`** | Mate XT | 附图 / 玄黑 | 800x800 |
+| **`goods_pc1.png`** | MateBook X Pro | 主图 / 晴蓝 | 800x800 |
+| **`goods_pc2.png`** | MateBook X Pro | 附图 / 砚黑 | 800x800 |
+| **`goods_pad1.png`** | MatePad Pro | 主图 / 雅川青 | 800x800 |
+| **`goods_pad2.png`** | MatePad Pro | 附图 / 曜金黑 | 800x800 |
+
+## 5. 营销图片 (Marketing)
+| 图片文件名 | 用途描述 | 建议尺寸 |
+| :--- | :--- | :--- |
+| **`banner1.png`** | 首页轮播图 - 新品首发 | 1080x420 |
+| **`banner_home.png`** | 首页轮播图 - 智慧生活节 | 1080x420 |
+| **`topic1.png`** | 专题封面图 - 鸿蒙精品 | 600x300 |
+| **`topic_home.png`** | 专题封面图 - 打造智慧家 | 600x300 |
